0c4a241fb2bc75da4f6c69830e9c5207c40b84b1
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2026-02-12 22:58:47 -0800

    Finished exercise

--- src/ex02.ts
+++ src/ex02.ts
@@ -1,11 +1,12 @@
-// 2. Create a generic interface called `Data` with two properties: 
-// `id` (number) and `value` (of any type). 
+// 2. Create a generic interface called `Data` with two properties:
+// `id` (number) and `value` (of any type).
 // Define an object of type `Data` where `value` is a string "Generic" and `id` is 1.
 // Log the object.
 
-interface Data {
-  
+interface Data<T> {
+  id: number;
+  value: T;
 }
 
-const data: Data = { id: 1, value: "Generic" };
+const data: Data<string> = { id: 1, value: "Generic" };
 console.log(data); // Expected output: { id: 1, value: "Generic" }
